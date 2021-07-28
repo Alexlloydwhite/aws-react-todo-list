70023b0b532b0d70aa9a2eee67ac582ba0bae8a1
Alex White <alexlloydwhite@gmail.com>
2021-07-28 17:21:23 -0500

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,63 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface TodoState {
+  todoList: {
+    completed: boolean,
+    createdAt: string,
+    id: string,
+    todo: string
+  }[]
+}
+
+export default function App() {
+  const [listOfTodos, setListOfTodos] = useState<TodoState["todoList"]>([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const getListOfTodos = () => {
+    axios.get('https://oajwgks9xh.execute-api.us-east-2.amazonaws.com/dev/todos')
+      .then((result) => {
+        setListOfTodos(result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    getListOfTodos();
+  }, []);
+
+  const addTodo = (e: any) => {
+    e.preventDefault();
+
+    if (newTodo) {
+      axios.post('https://oajwgks9xh.execute-api.us-east-2.amazonaws.com/dev/', { todo: newTodo })
+        .then(() => {
+          getListOfTodos();
+          setNewTodo('');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={addTodo}>
+        <input
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button type="submit">add</button>
+      </form>
+      {listOfTodos.map((todo) => (
+        <h4 key={todo.id}>
+          {todo.todo}
+        </h4>
+      ))}
+    </div>
+  );
+}
