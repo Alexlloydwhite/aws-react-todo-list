a4a12887d535c451545d622f4b19de49a7f5ad06
Alex White <alexlloydwhite@gmail.com>
2021-08-04 12:07:18 -0500

    formatting data as a table

--- src/components/TodoTable/TodoTable.tsx
+++ src/components/TodoTable/TodoTable.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../redux/reducers/_rootReducer';
+import './TodoTable.css';
 
 export default function TodoTable() {
     const dispatch = useDispatch();
@@ -17,11 +18,26 @@ export default function TodoTable() {
 
     return (
         <div>
-            {todoList.map((todo: any) => (
-                <h4 key={todo.id}>
-                    {todo.todo}
-                </h4>
-            ))}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Task</th>
+                        <th>Created</th>
+                        <th>Completed?</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {todoList.map((todo: any) => (
+                        <tr key={todo.id}>
+                            <td>{todo.todo}</td>
+                            <td>{todo.createdAt}</td>
+                            <td><button>Complete</button></td>
+                            <td><button>Delete</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
