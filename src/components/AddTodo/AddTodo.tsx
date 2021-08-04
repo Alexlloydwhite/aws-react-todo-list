65baeca73a67b147ab2783c4fc0291748ce3286b
Alex White <alexlloydwhite@gmail.com>
2021-08-04 08:10:39 -0500

    adding module with local state to add new todo

---
+++ src/components/AddTodo/AddTodo.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export default function AddTodo() {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const submitTodo = () => {
+        console.log('Clicked submit!');
+    }
+
+    return (
+        <form onSubmit={submitTodo}>
+            <input
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+        </form>
+    );
+}
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import TodoTable from '../TodoTable/TodoTable';
+import AddTodo from '../AddTodo/AddTodo';
 
 export default function App() {
   
 
   return (
     <div className="App">
+      <AddTodo />
       <TodoTable />
     </div>
   );
