9663ee3cde8556487ae9a80180a20daed66be63a
Alex White <alexlloydwhite@gmail.com>
2021-08-10 11:28:07 -0500

    init setup, delete modal component

---
+++ src/components/DeleteModal/DeleteModal.tsx
@@ -0,0 +1,11 @@
+import './DeleteModal.css';
+
+export default function DeleteModal() {
+    return (
+        <div className="modal-content">
+            <div className="container">
+                <h1>Modal Box</h1>
+            </div>
+        </div>
+    );
+}
