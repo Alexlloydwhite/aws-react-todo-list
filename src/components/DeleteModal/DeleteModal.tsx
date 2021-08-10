8a0b6a18f8e587b14fbc176e553675cce12d77a2
Alex White <alexlloydwhite@gmail.com>
2021-08-10 11:35:51 -0500

    adding CSS to modal

--- src/components/DeleteModal/DeleteModal.tsx
+++ src/components/DeleteModal/DeleteModal.tsx
@@ -2,9 +2,11 @@ import './DeleteModal.css';
 
 export default function DeleteModal() {
     return (
-        <div className="modal-content">
-            <div className="container">
-                <h1>Modal Box</h1>
+        <div className="modal">
+            <div className="modal-content">
+                <div className="container">
+                    <h1>Modal Box</h1>
+                </div>
             </div>
         </div>
     );
