b2c691c57965e6083373c6ba1d1a7c2d438b9e95
Alex White <alexlloydwhite@gmail.com>
2021-08-16 10:21:18 -0500

    removing load spinner for toggle task complete

--- src/redux/sagas/toggleComplete.ts
+++ src/redux/sagas/toggleComplete.ts
@@ -9,13 +9,11 @@ export function* toggleComplete() {
 
 export function* createWorker(action: any) {
     try {
-        yield put({ type: ActionType.loading });
         yield call(
             axios.put,
             `https://oajwgks9xh.execute-api.us-east-2.amazonaws.com/dev/todo/${action.id}`,
             { completed: action.payload }
         );
-        yield put({ type: ActionType.getListOfTodos });
     } catch (error) { 
         console.log(error); 
         yield put({ type: ActionType.loadingError });
