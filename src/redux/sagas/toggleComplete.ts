7dc72aa4df1fa74b24b15470ec145ae22a4b0ce3
Alex White <alexlloydwhite@gmail.com>
2021-08-18 12:20:20 -0500

    adding interface to toggleComplete saga

--- src/redux/sagas/toggleComplete.ts
+++ src/redux/sagas/toggleComplete.ts
@@ -1,20 +1,31 @@
 import { takeEvery, call, put } from "@redux-saga/core/effects";
-import { ActionType } from '../action-types/index';
-import axios from 'axios';
+import { ActionType } from "../action-types/index";
+import axios from "axios";
+import { Action } from "redux";
+import { StrictEffect } from "redux-saga/effects";
 
-export function* toggleComplete() {
-    yield takeEvery(ActionType.toggleComplete, createWorker);
+interface PayloadAction extends Action, IToggleComplete {
+  type: ActionType.toggleComplete;
 }
 
-export function* createWorker(action: any) {
-    try {
-        yield call(
-            axios.put,
-            `https://oajwgks9xh.execute-api.us-east-2.amazonaws.com/dev/todo/${action.id}`,
-            { completed: action.payload }
-        );
-    } catch (error) { 
-        console.log(error); 
-        yield put({ type: ActionType.loadingError });
-    }
+interface IToggleComplete {
+  id: string;
+  payload: boolean;
+}
+
+export function* toggleComplete(): Generator<StrictEffect> {
+  yield takeEvery(ActionType.toggleComplete, createWorker);
+}
+
+export function* createWorker(action: PayloadAction) {
+  try {
+    yield call(
+      axios.put,
+      `https://oajwgks9xh.execute-api.us-east-2.amazonaws.com/dev/todo/${action.id}`,
+      { completed: action.payload }
+    );
+  } catch (error) {
+    console.log(error);
+    yield put({ type: ActionType.loadingError });
+  }
 }
