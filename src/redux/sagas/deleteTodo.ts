e0613ca0d6e25e950b47ecbcda99e64554d2c8fa
Alex White <alexlloydwhite@gmail.com>
2021-08-18 12:16:03 -0500

    adding interface to deleteTask

--- src/redux/sagas/deleteTodo.ts
+++ src/redux/sagas/deleteTodo.ts
@@ -1,22 +1,32 @@
 import { takeEvery, call, put } from "@redux-saga/core/effects";
-import { ActionType } from '../action-types/index';
-import axios from 'axios';
+import { ActionType } from "../action-types/index";
+import axios from "axios";
+import { Action } from "redux";
+import { StrictEffect } from "redux-saga/effects";
 
-export function* deleteTodo() {
-    yield takeEvery(ActionType.deleteTask, createWorker);
+interface DeleteAction extends Action, IDeleteTodo {
+  type: ActionType.deleteTask;
 }
 
-export function* createWorker(action: any) {
-    try {
-        yield put({ type: ActionType.loading });
-        yield call(
-            axios.delete, 
-            `https://oajwgks9xh.execute-api.us-east-2.amazonaws.com/dev/todo/${action.id}`
-        );
-        yield put({ type: ActionType.getListOfTodos });
-        yield put({ type: ActionType.loadingSuccess });
-    } catch (error) { 
-        console.log(error);
-        yield put({ type: ActionType.loadingError });
-    }
+interface IDeleteTodo {
+  id: string;
+}
+
+export function* deleteTodo(): Generator<StrictEffect> {
+  yield takeEvery(ActionType.deleteTask, createWorker);
+}
+
+export function* createWorker(action: DeleteAction) {
+  try {
+    yield put({ type: ActionType.loading });
+    yield call(
+      axios.delete,
+      `https://oajwgks9xh.execute-api.us-east-2.amazonaws.com/dev/todo/${action.id}`
+    );
+    yield put({ type: ActionType.getListOfTodos });
+    yield put({ type: ActionType.loadingSuccess });
+  } catch (error) {
+    console.log(error);
+    yield put({ type: ActionType.loadingError });
+  }
 }
