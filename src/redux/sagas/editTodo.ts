c25c917e43da46e7a27353dc37e5cc32aee70e42
Alex White <alexlloydwhite@gmail.com>
2021-08-18 12:18:12 -0500

    adding interfaces to editTodo

--- src/redux/sagas/editTodo.ts
+++ src/redux/sagas/editTodo.ts
@@ -1,22 +1,33 @@
 import { takeEvery, call, put } from "@redux-saga/core/effects";
-import { ActionType } from '../action-types/index';
-import axios from 'axios';
+import { ActionType } from "../action-types/index";
+import { Action } from "redux";
+import axios from "axios";
+import { StrictEffect } from "redux-saga/effects";
 
-export function* editTodo() {
-    yield takeEvery(ActionType.editTask, createWorker);
+interface PayloadAction extends Action, IEditTodo {
+  type: ActionType.editTask;
 }
 
-export function* createWorker(action: any) {
-    try {
-        yield put({ type: ActionType.loading });
-        yield call(
-            axios.put,
-            `https://oajwgks9xh.execute-api.us-east-2.amazonaws.com/dev/todo/edit/${action.id}`,
-            { editedTodo: action.edit }
-        );
-        yield put({ type: ActionType.getListOfTodos });
-    } catch (error) { 
-        console.log(error); 
-        yield put({ type: ActionType.loadingError });
-    }
+interface IEditTodo {
+  id: string;
+  edit: string;
+}
+
+export function* editTodo(): Generator<StrictEffect> {
+  yield takeEvery(ActionType.editTask, createWorker);
+}
+
+export function* createWorker(action: PayloadAction) {
+  try {
+    yield put({ type: ActionType.loading });
+    yield call(
+      axios.put,
+      `https://oajwgks9xh.execute-api.us-east-2.amazonaws.com/dev/todo/edit/${action.id}`,
+      { editedTodo: action.edit }
+    );
+    yield put({ type: ActionType.getListOfTodos });
+  } catch (error) {
+    console.log(error);
+    yield put({ type: ActionType.loadingError });
+  }
 }
